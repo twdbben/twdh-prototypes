b88b6ded9c2aa2182abae9344a14c1c4f5088fb3
Ben Bright <ben.bright@twdb.texas.gov>
2022-08-03 15:41:30 -0500

    Page Details; Modals; Tab Panels; Missing Fields
    Fleshed out Details, Questions, Validation Criteria and
    Activity Stream on all 6 pages
    
    Added Bootstrap modals and tab interfaces
    
    Completed some missing fields

--- js/twdh.js
+++ js/twdh.js
@@ -1,5 +1,13 @@
 
-$('#spatial-tabs a').on('click', function (e) {
+$('#add-resource-tabs a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
+$('#coordinate-system-tabs a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
+$('#coordinate-field-tabs a').on('click', function (e) {
     e.preventDefault()
     $(this).tab('show')
 })
